31a2fd21175ba9af09903ea09aaac9e4d36b95f1
plq001 <286050295@qq.com>
2022-10-26 23:32:59 +0800

    Update ycyhq.js

--- ycyhq.js
+++ ycyhq.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         淘宝天猫隐藏优惠券
+// @name         【最新可用！】淘宝天猫隐藏优惠券
 // @namespace    http://tbtmycyhq.yhq
-// @version      0.1
+// @version      1.3
 // @description  淘宝/天猫购物助手，在浏览淘宝天猫的时候自动显示当前商品是否有粉丝福利购优惠(内部优惠券、隐藏优惠券、内部券、隐藏券)，让你买的比别人更便宜！
 // @author       无声风雪
 // @match             *://*.taobao.com/*
@@ -27,9 +27,10 @@
 // @connect *
 // @require           https://cdn.bootcdn.net/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require           https://cdn.bootcdn.net/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js
-// @antifeature       referral-link 【应GreasyFork代码规范要求：含有优惠券查询功能的脚本必须添加此提示！在此感谢大家的理解...】
+// @license MIT
+// @antifeature       referral-link 【按照规范要求，本脚本因包含查询优惠券需要添加此提示，感谢大家支持】
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
     var style = document.createElement('link');
@@ -40,26 +41,39 @@
     var serv = 'https://qxm.iwapan.com/yhq_chajian_js.aspx?ly=yh&';
     var url = location.href;
     var time = 50;
-
+ 
      if (url.indexOf("//item.taobao.com/item") != -1 || url.indexOf("//detail.tmall.com/item") != -1 || url.indexOf("//chaoshi.detail.tmall.com/item") != -1 || url.indexOf("//detail.tmall.hk/hk/item") != -1 || url.indexOf("//detail.tmall.hk/item") != -1) {
-
+ 
          function QueryString(item){
             var sValue = location.search.match(new RegExp("[\?\&]"+item+"=([^\&]*)(\&?)", "i"))
             return sValue?sValue[1]:sValue
         }
-
+ 
         $.get(serv+'tb_shangpin_id='+QueryString("id"), function(data) {
-            var obj=$.parseJSON(data);
-            console.log(obj.yhq[0].yhq_url);
             var dis_res_html="";
+            if(data!="null"){
+            var obj=$.parseJSON(data);
+           // console.log(obj.yhq[0].yhq_url);
           dis_res_html=dis_res_html+"<div class='qxm_yhq'>";
 	 dis_res_html=dis_res_html+"<a style='font-size: 10px;color:#fff' target='_blank'  href='"+obj.yhq[0].yhq_url+"'><div class='par'><span>"+obj.yhq[0].yhq_name+"</span></div></a>";
 	 dis_res_html=dis_res_html+"<a style='font-size: 10px;color:#fff' target='_blank'  href='"+obj.yhq[0].yhq_url+"'>"+"<div class='copy'>点击领取</div></a>";
 	 dis_res_html=dis_res_html+"";
 	 dis_res_html=dis_res_html+"</div>";
-
+            }else
+            {
+                dis_res_html=dis_res_html+"<div class='qxm_yhq'>";
+	 dis_res_html=dis_res_html+"<a style='font-size: 10px;color:#fff' target='_blank'  href='#'><div class='par'><span>未找到优惠券</span></div></a>";
+	 dis_res_html=dis_res_html+"<a style='font-size: 10px;color:#fff' target='_blank'  href='#'><div class='copy'></div></a>";
+	 dis_res_html=dis_res_html+"";
+	 dis_res_html=dis_res_html+"</div>";
+            }
       setTimeout( function () {
-                $('.skuWrapper').after(dis_res_html);
+             if(document.getElementById('J_isku')){$('.J_isku').after(dis_res_html);}
+             if(document.getElementById('tb-key')){$('.tb-key').after(dis_res_html);}
+             if(document.getElementById('skuWrapper')){ $('.skuWrapper').after(dis_res_html);}
+             if(document.getElementsByClassName('J_isku')){$('.J_isku').after(dis_res_html);}
+             if(document.getElementsByClassName('tb-key')){$('.tb-key').after(dis_res_html);}
+             if(document.getElementsByClassName('skuWrapper')){ $('.skuWrapper').after(dis_res_html);}
             }, 1000);
         })
     }
